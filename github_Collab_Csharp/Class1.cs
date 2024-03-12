4bca8b0ede90af3b9da52367665ff37bf17b61b7
GiftedR <kidsgamers@hotmail.com>
2024-03-12 11:31:53 -0700

    Started DiceRoller Class

---
+++ github_Collab_Csharp/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github_Collab_Csharp
+{
+    internal class DiceRoller
+    {
+        public DiceRoller() { }
+        public int rollNewNumber(int max)
+        {
+            return 0;
+        }
+    }
+}
